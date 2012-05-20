b0f4f9c06f7eff2312ece1a9d44c9316b7889d38
Projects and Stuff <support@projectsandstuff.com>
2012-05-20 01:21:59 -0400

    Added Atmel Studio Project skeleton

---
+++ Firmware/Chron.c
@@ -0,0 +1,20 @@
+/*
+ * Chron.c
+ * Chron - A digitally controlled analog clock kit
+ *
+ * Projects and Stuff LLC
+ *
+ * Created: 5/20/2012 1:16:14 AM
+ * Author: Jack Linke
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //
+    }
+}
